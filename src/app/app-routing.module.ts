e193cb768eedd7a65ffe10c28b290182e647c47d
Rocio Llanes <rollanes13@gmail.com>
2022-10-18 18:52:00 -0300

    se agregan componentes modulo pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ServicesComponent } from './pages/services/services.component';
 
-const routes: Routes = []
+const routes: Routes = [
+  {path: 'home', component:ServicesComponent},
+  {path: 'about-us', component:AboutUsComponent}
+]
 ;
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ServicesComponent } from './services/services.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { RouterLinkWithHref, RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ServicesComponent,
+    AboutUsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    ServicesComponent,
+    AboutUsComponent
   ]
 })
 export class PagesModule { }
